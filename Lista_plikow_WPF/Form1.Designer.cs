6be2c555846ba6c30dfd8ab0ae3d47ee2b957c95
unknown <mati199111@gmail.com>
2020-01-04 00:12:10 +0100

    04012020

---
+++ Lista_plikow_WPF/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace Lista_plikow_WPF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.przywróćToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.zamknijToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(12, 348);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(180, 84);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(12, 54);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(758, 276);
+            this.listBox1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(198, 348);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(180, 84);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Koniec";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.label1.Location = new System.Drawing.Point(21, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(724, 23);
+            this.label1.TabIndex = 4;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.Location = new System.Drawing.Point(384, 348);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(180, 84);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Bieżący folder";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button4.Location = new System.Drawing.Point(570, 348);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(180, 84);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "CE";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button5.Location = new System.Drawing.Point(670, 12);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Schowaj";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.przywróćToolStripMenuItem,
+            this.zamknijToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(211, 80);
+            // 
+            // przywróćToolStripMenuItem
+            // 
+            this.przywróćToolStripMenuItem.Name = "przywróćToolStripMenuItem";
+            this.przywróćToolStripMenuItem.Size = new System.Drawing.Size(210, 24);
+            this.przywróćToolStripMenuItem.Text = "Przywróć";
+            this.przywróćToolStripMenuItem.Click += new System.EventHandler(this.przywróćToolStripMenuItem_Click);
+            // 
+            // zamknijToolStripMenuItem
+            // 
+            this.zamknijToolStripMenuItem.Name = "zamknijToolStripMenuItem";
+            this.zamknijToolStripMenuItem.Size = new System.Drawing.Size(210, 24);
+            this.zamknijToolStripMenuItem.Text = "Zamknij";
+            this.zamknijToolStripMenuItem.Click += new System.EventHandler(this.zamknijToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(782, 453);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.MaximumSize = new System.Drawing.Size(800, 500);
+            this.Name = "Form1";
+            this.Text = "Lista_Plików";
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem przywróćToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem zamknijToolStripMenuItem;
+    }
+}
+
---
+++ Lista_plikow_WPF/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Lista_plikow_WPF
+{
+    public partial class Form1 : Form
+    {
+        #region inicjalizacja formsa
+        public Form1()
+        {
+            InitializeComponent();
+            label1.Text = ("Witaj w przeglądarce plików, aby rozpocząć kliknij start :>");
+        }
+        #endregion
+        #region przycisk start folderbrowser
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog FBD = new FolderBrowserDialog();
+            if (FBD.ShowDialog() == DialogResult.OK)
+            {
+                listBox1.Items.Clear();
+                string[] files = Directory.GetFiles(FBD.SelectedPath, "*.exe", SearchOption.TopDirectoryOnly);
+                string[] files2 = Directory.GetFiles(FBD.SelectedPath, "*.lnk", SearchOption.TopDirectoryOnly);
+                string[] dirs = Directory.GetDirectories(FBD.SelectedPath);
+
+                foreach(string file in files)
+                {
+                    listBox1.Items.Add(Path.GetFileName(file));
+                }
+                foreach (string file in files2)
+                {
+                    listBox1.Items.Add(Path.GetFileName(file));
+                }
+            }
+        }
+        #endregion
+        #region zamykanie 
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+        #endregion
+        #region test
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        #endregion
+        #region biezacy katalog
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string dir = Directory.GetCurrentDirectory();
+            string[] file = Directory.GetFiles(dir, "*.exe", SearchOption.TopDirectoryOnly);
+            string[] file2 = Directory.GetFiles(dir, "*.lnk", SearchOption.TopDirectoryOnly);
+
+            foreach(string first in file)
+            {
+                listBox1.Items.Add(Path.GetFileName(first));
+            }
+            foreach (string second in file2)
+            {
+                listBox1.Items.Add(Path.GetFileName(second));
+            }
+        }
+        #endregion
+        #region czyszczenie listboxa
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.listBox1.Items.Clear();
+        }
+        #endregion
+        #region dodanie notifyicona + toolstripmenu
+        /// <summary>
+        /// z przybornika nalezy wybraz notifyicon i strip menu item, nastepnie dodac opcje przywroc i zamknij w liscie rozwijanej
+        /// zaimplementowac funkcjonalnosci
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button5_Click(object sender, EventArgs e)
+        {
+            notifyIcon1.Visible = true;
+            notifyIcon1.Text = "Schowaj";
+            notifyIcon1.Icon = this.Icon;
+            notifyIcon1.ContextMenuStrip = contextMenuStrip1;
+            this.ShowInTaskbar = false;
+            this.Visible = false;
+        }
+
+        private void przywróćToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.ShowInTaskbar = true;
+            this.Visible = true;
+            notifyIcon1.Visible = false;
+        }
+
+        private void zamknijToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        #endregion
+    }
+}
